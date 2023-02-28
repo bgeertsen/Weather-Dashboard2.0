af4b7c2ca6a6acdf6018ee217b455299a65dd5ac
bg <62928419+bgeertsen@users.noreply.github.com>
2023-02-27 20:33:37 -0800

    added video background

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,8 +1,13 @@
-import React from "react";
-import "./Login.css";
+import React from 'react';
+import backgroundVid from '../../assets/pexels-arthouse-studio-8494852.mp4';
+import './Login.css';
 
 const Login = () => {
-  return <div>Hello world!</div>;
+    return (
+        <video autoPlay loop muted>
+            <source src={backgroundVid} type="video/mp4"></source>
+        </video>
+    );
 };
 
 export default Login;
